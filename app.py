665140139b505cdf7c02236df8d111a3a3a08ed9
anavarro81 <anavarro67@hotmail.com>
2023-04-26 10:52:17 +0200

    primer commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+# Crea la instancia de la aplicación
+app=Flask(__name__)
+
+# Crea la vista index, URL principal
+@app.route('/')
+def index():
+	return "<h1> ¡¡¡ Bienvenido a Flask !!! <h1>"
+
+# Ejecutamos la aplicación. 
+if __name__ == '__main__':
+    app.run()
